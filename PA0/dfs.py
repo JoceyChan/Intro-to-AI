9093ef3b26dd55e4e92ca6d96ec11e1e9b483c50
Jocey Chan <joceychan331718@gmail.com>
2021-10-07 19:03:52 -0700

    DFS

--- PA0/dfs.py
+++ PA0/dfs.py
@@ -7,9 +7,9 @@
 # Frontier.
 # 
 # YOUR COMMENTS INCLUDING CITATIONS
-#
+# TA Liza Oh Office Hours, Pseudocode for bfs and dfs(Artificial Intelligence, A modern approach), Python Documentation (docs.python.org/3/tutorial/)
 # YOUR NAME - THE DATE
-# 
+# Jocelyn Chan October 7th 2021
 
 
 from route import Node
@@ -24,4 +24,41 @@ def DFS(problem, repeat_check=False):
 
     # PLACE YOUR CODE HERE
 
+    # Initialize the node with the starting location
+    node = Node(problem.start)
+
+    # If the start location is the goal then return node
+    if problem.is_goal(node.loc):
+        return node
+
+    # Stack keeps track of unvisited nodes
+    frontier = Frontier(node, stack=True)
+
+    # We keep track of the visited nodes
+    visited_nodes = [problem.start]
+
+    # We continue moving in the tree layers until all nodes visited
+    while not frontier.is_empty():
+        # pop the node child from the queue since it will be visited
+        node = frontier.pop()
+        # visit every neighbor of the current node
+        # node.expand(problem) returns list reachable nodes of current node
+        if problem.is_goal(node.loc):
+            return node
+        for node_child in node.expand(problem):
+            # Get location of child
+            s = node_child.loc
+            # If child node location is goal return node
+            # If not visited...
+            if repeat_check:
+                if s not in visited_nodes:
+                    # Register node as visited node to avoid inf loop
+                    visited_nodes.append(s)
+                    # This to later check reachable nodes and keep traversing tree
+                    frontier.add(node_child)
+            else:
+                frontier.add(node_child)
+    # We keep track of the visited nodes
+    visited_nodes = [problem.start]
+    # Return None if start cannot reach end
     return None
